fc047bbadf64318d0cfe9a1a57b26bead1d9e314
carlos <carlosdummy14@gmail.com>
2022-02-14 22:35:19 -0600

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0
+  }
+}
