2b22ff765daf0e759e13d535acdccfaf28e3179c
carlos <carlosdummy14@gmail.com>
2022-02-22 22:01:09 -0600

    Fit text and tips

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,7 +3,7 @@ function Question ({ question, handleOptionClick, gameover }) {
     let optionClass = 'option'
     if (!gameover && item === question.selected) { optionClass += ' option-selected' }
     if (gameover) {
-      if (item === question.selected && item === question.correct) {
+      if (item === question.correct) {
         optionClass += ' option-correct'
       } else if (item === question.selected && item !== question.answare) {
         optionClass += ' option-wrong option-disable'
@@ -11,14 +11,19 @@ function Question ({ question, handleOptionClick, gameover }) {
         optionClass += ' option-disable'
       }
     }
+
+    const title = item
+    const itemText = item.length > 20 ? item.substr(0, 20) + '...' : item
+
     return gameover
-      ? <button className={optionClass} key={item}> {item} </button>
-      : <button className={optionClass} key={item} onClick={() => handleOptionClick(question.id, item)} > {item} </button>
+      ? <button className={optionClass} key={item} title={title}> {itemText} </button>
+      : <button className={optionClass} key={item} title={title} onClick={() => handleOptionClick(question.id, item)} > {itemText} </button>
   })
 
+  const styleText = question.text.length > 60 ? { fontSize: '0.8rem' } : {}
   return (
     <div className="question">
-      <h3 className="question-text">{question.text}</h3>
+      <h3 className="question-text" style={ styleText }>{question.text}</h3>
       <div className="options">{optionsToDraw}</div>
     </div>
   )
